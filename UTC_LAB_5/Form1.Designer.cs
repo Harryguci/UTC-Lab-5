b63078fc1228b5a676504520b5a39f3e77474d1b
Harryguci <huyquang0028@gmail.com>
2023-10-24 19:05:39 +0700

    Create project

---
+++ UTC_LAB_5/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace UTC_LAB_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.txtDiemThi = new System.Windows.Forms.TextBox();
+            this.txtSoTin = new System.Windows.Forms.TextBox();
+            this.txtTenMon = new System.Windows.Forms.TextBox();
+            this.txtMaMon = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnThem);
+            this.groupBox1.Controls.Add(this.btnXoa);
+            this.groupBox1.Controls.Add(this.btnThongKe);
+            this.groupBox1.Controls.Add(this.btnLamMoi);
+            this.groupBox1.Controls.Add(this.txtDiemThi);
+            this.groupBox1.Controls.Add(this.txtSoTin);
+            this.groupBox1.Controls.Add(this.txtTenMon);
+            this.groupBox1.Controls.Add(this.txtMaMon);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin điểm thi ";
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(39, 322);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(115, 55);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Thêm vào danh sách";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(237, 322);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(97, 55);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoá thông tin";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(410, 322);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(89, 55);
+            this.btnThongKe.TabIndex = 6;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.Location = new System.Drawing.Point(410, 52);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(89, 63);
+            this.btnLamMoi.TabIndex = 7;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = true;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
+            // 
+            // txtDiemThi
+            // 
+            this.txtDiemThi.Location = new System.Drawing.Point(116, 225);
+            this.txtDiemThi.Name = "txtDiemThi";
+            this.txtDiemThi.Size = new System.Drawing.Size(184, 22);
+            this.txtDiemThi.TabIndex = 3;
+            // 
+            // txtSoTin
+            // 
+            this.txtSoTin.Location = new System.Drawing.Point(116, 164);
+            this.txtSoTin.Name = "txtSoTin";
+            this.txtSoTin.Size = new System.Drawing.Size(184, 22);
+            this.txtSoTin.TabIndex = 2;
+            // 
+            // txtTenMon
+            // 
+            this.txtTenMon.Location = new System.Drawing.Point(116, 110);
+            this.txtTenMon.Name = "txtTenMon";
+            this.txtTenMon.Size = new System.Drawing.Size(184, 22);
+            this.txtTenMon.TabIndex = 1;
+            // 
+            // txtMaMon
+            // 
+            this.txtMaMon.Location = new System.Drawing.Point(116, 52);
+            this.txtMaMon.Name = "txtMaMon";
+            this.txtMaMon.Size = new System.Drawing.Size(184, 22);
+            this.txtMaMon.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Điểm thi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Số tín";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên môn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã môn";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgv);
+            this.groupBox2.Location = new System.Drawing.Point(583, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(615, 425);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh sách các môn";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(19, 22);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(590, 397);
+            this.dgv.TabIndex = 0;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kết thúc học phần KTLTTQ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSoTin;
+        private System.Windows.Forms.TextBox txtTenMon;
+        private System.Windows.Forms.TextBox txtMaMon;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Button btnLamMoi;
+        private System.Windows.Forms.TextBox txtDiemThi;
+    }
+}
---
+++ UTC_LAB_5/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System.Data;
+
+namespace UTC_LAB_5
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(Local);Initial Catalog=MonThi;User ID=sa;Password=123456");
+        SqlCommand cmd;
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Load_Data()
+        {
+            cmd = con.CreateCommand();
+            cmd.CommandText = "select * from monhoc";
+            da.SelectCommand = cmd;
+            dt.Clear();
+            da.Fill(dt);
+            dgv.DataSource = dt;
+            dgv.Columns["mamon"].HeaderText = "Mã môn";
+            dgv.Columns["mamon"].Width = 100;
+            dgv.Columns["tenmon"].HeaderText = "Tên môn";
+            dgv.Columns["tenmon"].Width = 100;
+            dgv.Columns["sotin"].HeaderText = "Số tín chỉ";
+            dgv.Columns["sotin"].Width = 100;
+            dgv.Columns["diemthi"].HeaderText = "Điểm thi";
+            dgv.Columns["diemthi"].Width = 100;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            Load_Data();
+            con.Close();
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            txtDiemThi.Text = string.Empty;
+            txtMaMon.Text = string.Empty;
+            txtSoTin.Text = string.Empty;
+            txtTenMon.Text = string.Empty;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (txtMaMon.Text != string.Empty && txtTenMon.Text != string.Empty
+                && txtSoTin.Text != string.Empty && txtDiemThi.Text != string.Empty)
+            {
+                con.Open();
+                using (SqlCommand command = con.CreateCommand())
+                {
+                    int sotin = Convert.ToInt32(txtSoTin.Text);
+                    double diemthi = Convert.ToDouble(txtDiemThi.Text);
+
+                    command.CommandText = "SELECT COUNT(*) FROM monhoc WHERE mamon = @mamon";
+                    command.Parameters.AddWithValue("@mamon", txtMaMon.Text);
+                    int duplicateCount = (int)command.ExecuteScalar();
+
+                    if (duplicateCount > 0)
+                    {
+                        MessageBox.Show("Mã môn đã tồn tại. Vui lòng chọn mã môn khác.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        command.CommandText = "INSERT INTO monhoc (mamon, tenmon, sotin, diemthi) VALUES (@mamon, @tenmon, @sotin, @diemthi)";
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@mamon", txtMaMon.Text);
+                        command.Parameters.AddWithValue("@tenmon", txtTenMon.Text);
+                        command.Parameters.AddWithValue("@sotin", sotin);
+                        command.Parameters.AddWithValue("@diemthi", diemthi);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Thêm dữ liệu thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Load_Data();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng điền đủ các trường thông tin.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            con.Close();
+        }
+
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (txtMaMon.Text != string.Empty && txtTenMon.Text != string.Empty
+                && txtSoTin.Text != string.Empty && txtDiemThi.Text != string.Empty)
+            {
+                if (MessageBox.Show("Bạn có chắc chắn xoá?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    con.Open();
+                    cmd = con.CreateCommand();
+                    int mamon = Convert.ToInt32(txtMaMon.Text);
+                    cmd.CommandText = "DELETE FROM monhoc WHERE mamon = @mamon";
+                    cmd.Parameters.AddWithValue("@mamon", mamon);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Xoá dữ liệu thành công.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Load_Data();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn các trường thông tin muốn xoá.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow selectedRow = dgv.Rows[e.RowIndex];
+                txtMaMon.Text = selectedRow.Cells[0].Value.ToString();
+                txtTenMon.Text = selectedRow.Cells[1].Value.ToString();
+                txtSoTin.Text = selectedRow.Cells[2].Value.ToString();
+                txtDiemThi.Text = selectedRow.Cells[3].Value.ToString();
+            }
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            using (SqlCommand command = con.CreateCommand())
+            {
+                command.CommandText = "SELECT SUM(diemthi * sotin) FROM monhoc";
+                double TongDiem = Convert.ToDouble(command.ExecuteScalar());
+
+                command.CommandText = "SELECT SUM(sotin) FROM monhoc";
+                int TongSoTin = Convert.ToInt32(command.ExecuteScalar());
+
+                double DTB = TongDiem / TongSoTin;
+
+                MessageBox.Show($"Điểm trung bình: {DTB}", "Kết quả", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            con.Close();
+        }
+    }
+}
+}
